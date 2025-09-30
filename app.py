03fa8c3572693a8304c6512d033921d1a8e0e09e
0255962-star <0255962@up.edu.mx>
2025-09-30 17:24:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 import streamlit as st
 import yfinance as yf
 
-st.set_page_config(page_title="Consulta de Acciones - Huizar", page_icon="📊")
+st.set_page_config(page_title="Consulta de Acciones - Gascon", page_icon="📊")
 
 # ======== Config clave desde Secrets ========
 API_KEY = st.secrets.get("GOOGLE_API_KEY", os.getenv("GOOGLE_API_KEY"))
